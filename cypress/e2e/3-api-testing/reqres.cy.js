d1d417bd7f44436f1638df028693962147b87f0a
elvanisa <elvanisamuhsina@gmail.com>
2023-10-20 09:05:47 +0700

    add cypress folder

---
+++ cypress/e2e/3-api-testing/reqres.cy.js
@@ -0,0 +1,10 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.request({
+      method: 'GET',
+      url: 'https://reqres.in/api/users?page=2'
+    }).then((response) => {
+      expect(response.status).to.equal(200)
+    })
+  })
+})
